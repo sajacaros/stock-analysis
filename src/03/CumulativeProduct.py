966309b6feba9f6b5605d75395b891137e24a9b8
dukim <sajacall82@naver.com>
2022-04-12 21:19:08 +0900

    ch03 update

--- src/03/CumulativeProduct.py
+++ src/03/CumulativeProduct.py
@@ -4,6 +4,7 @@ import yfinance as yf
 yf.pdr_override()
 
 sec = pdr.get_data_yahoo('005930.KS', start='2018-05-04')
+
 sec_dpc = (sec['Close'] - sec['Close'].shift(1)) / sec['Close'].shift(1) * 100
 sec_dpc.iloc[0] = 0  # 일간 변동률의 첫 번째 값인 NaN을 0으로 변경한다.
 sec_dpc_cp = ((100 + sec_dpc) / 100).cumprod() * 100 - 100  # 일간 변동률 누적곱 계산
